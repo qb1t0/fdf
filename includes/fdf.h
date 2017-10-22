153cf2eef90f55ddd698c7fc241d3419b0526073
Yaroslav Salata <yaroslavsalata@gmail.com>
2017-10-22 16:46:19 +0300

    start parsing input file

--- includes/fdf.h
+++ includes/fdf.h
@@ -4,15 +4,23 @@
 # include "../libft/libft.h"
 # include "../minilibx/mlx.h"
 # include "../libft/ft_printf/ft_printf.h"
+# define HEX "01234567890abcdef"
 
 static char *g_e[] = {
         "Usage: ./fdf [-c] <mapname.fdf>",
         "Can't open() file",
         "Can't read() file",
-        "Invalid map"
+        "Invalid coordinate",
+        "Empty file",
+        ""
 };
 
 typedef struct s_map{
     int        size;
 }              t_map;
+
+typedef struct s_o{
+    int        z;       //z-coord
+    int        c;       //c-color
+}               t_o;
 #endif //FDF_TEST_FDF_H
--- main.c
+++ main.c
@@ -12,39 +12,76 @@ int fdf_error(int type)
     return (0);
 }
 
-int     fdf_checkstr(char *s)
+int     fdf_checkstr(int x, int minus, char *str)
 {
     int i;
-    char **str;
+    int j;
 
     i = 0;
-    str = ft_strsplit(s, ' ');
     while (str[i])
+    {
+        j = 0;
+        minus = str[i] == '-' && i++ ? 1 : 0;
+        while(ft_isdigit(str[i]) || str[i][j] != ',')
+            j++;
+        if (str[i][j] && str[i][j] == ',' ? j++ : exit(fdf_error(4)))
+        {
+            j += str[i][j] == '0' && str[i][j + 1] == 'x' ? 2 : 0;
+
+        }
         ft_isaldigit(str[i]) ? i++ : exit(fdf_error(4));
+    }
 
 }
 
-t_map   *fdf_parse(int fd, int gnl)
+void    fdf_parse(int fd, t_map *m, t_o ***t)
+{
+    char *s;
+    int  y;
+    int  x;
+    char **str;
+    t_o  **fig;
+
+    fig = *t;
+    x = 0;
+    y = 0;
+    fig = (t_o **)malloc(sizeof(t_o *) * m->size);
+    while (get_next_line(fd, &s) > 0)
+    {
+        str = ft_strsplit(s, ' ');
+        while (s[y])
+        {
+
+            y++;
+        }
+        x++;
+    }
+}
+
+t_map   *fdf_open(int fd, int gnl, char *name)
 {
     t_map *m;
     char *s;
+    int  l;
 
-    fd <= 0 ? exit(fdf_error(2)) : 0;
-    m = (t_map *)malloc(sizeof(t_map));
+    l = (int)ft_strlen(name);
+    (l < 5 || ft_strcmp(name + l - 4, ".fdf")) ? exit(fdf_error(1)) : 0;
+    fd <= 0 ? exit(fdf_error(2)) : m = (t_map *)malloc(sizeof(t_map));
     m->size = 0;
     while ((gnl = get_next_line(fd, &s)) > 0)
-    {
-        s[0] && ft_isdigit(s[0]) ? fdf_checkstr(s) : exit(fdf_error(3));
-        ++m->size;
-    }
+        s[0] ? ++m->size : exit(fdf_error(4));
     gnl < 0 ? exit(fdf_error(3)) : 0;
 }
 
 int main(int ac, char **av)
 {
     t_map *map;
+    t_o   **t;
 
-    ac != 2 ? exit(fdf_error(1)) : fdf_parse(open(av[1], O_RDONLY), 0);
+    ac != 2 ? \
+     exit(fdf_error(1)) : map = fdf_open(open(av[1], O_RDONLY), 0, av[0]);
+    map->size == 0 ? \
+        exit(fdf_error(5)) : fdf_parse(open(av[1], O_RDONLY), map, &t);
     printf("Hello, World!\n");
     return 0;
 }
